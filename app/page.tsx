c86b6d04d32fd1c8b16e3d59fdfcbd4bd01ae1b1
yamada <shinya.yamada@benext-corp.com>
2025-02-17 09:09:27 +0900

    💄 Update mobile layout of top page

--- app/page.tsx
+++ app/page.tsx
@@ -9,17 +9,17 @@ export default function Home() {
             麻雀スコア管理
           </div>
           <div className="font-light text-xl md:text-3xl text-gray-800 py-4 text-center">
-            対局を開始し、スコアを記録しましょう
+            対局したスコアを記録しましょう
           </div>
-          <div className="flex gap-4 mt-8">
+          <div className="flex flex-col md:flex-row gap-6 md:gap-4 mt-8 justify-center">
             <Link href="/games/new">
               <button className="bg-emerald-500 hover:bg-emerald-600 text-white font-bold py-3 px-6 rounded-full text-lg">
-                対局開始
+                対局を記録する
               </button>
             </Link>
             <Link href="/history">
               <button className="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-3 px-6 rounded-full text-lg">
-                対局履歴
+                対局履歴を見る
               </button>
             </Link>
           </div>
